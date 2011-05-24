dd05c6bc92e6b93b30cd681a04ae04025af279d1
Alvin Garcia <alvindgarcia@yahoo.com>
2011-05-24 11:14:55 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twitty::Application.config.secret_token = '85c6c4c5054084d589f16b15b8b4b886aabecbc108777f97781280e64eb4f5d3703dee2c9f7401a160f347a58ec0abd2cacbab863b88cce0815514712abb0193'
